583bf18336cb4b24e41aaab03d1033386903caff
Martin Sjåstad <m.sjastad@gmail.com>
2021-01-14 10:22:50 +0100

    remove commnets

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,16 +5,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   // mount: {},
-  plugins: [
-    ['@snowpack/plugin-babel'],
-    ['@snowpack/plugin-typescript'],
-    // {
-    // "input": ['.js', '.mjs', '.jsx', '.ts', '.tsx'], // (optional) specify files for Babel to transform
-    // transformOptions: {
-    // babel transform options
-    // }
-    // }
-  ],
+  plugins: [['@snowpack/plugin-babel'], ['@snowpack/plugin-typescript']],
   // installOptions: {},
   devOptions: { open: 'none', port: 8008 },
   // buildOptions: {},
