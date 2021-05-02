7db5154288012406a18da5c460f21fcf556897c2
Martin Sjåstad <m.sjastad@gmail.com>
2021-05-02 15:27:37 +0200

    Convert habitinput windi

--- src/components/HabitInput.svelte
+++ src/components/HabitInput.svelte
@@ -12,61 +12,21 @@
       clearInputHabit()
     }}
   >
-    <label for="habit">add habits you want to track</label>
-    <div class="flex">
+    <label for="habit" class="uppercase text-xs tracking-wide font-semibold"
+      >add habits you want to track</label
+    >
+    <div class="flex w-1/2 min-w-64 border-0 border-bottom-2 border-chino">
       <input
         bind:value={inputHabit}
         type="text"
         id="habit"
         placeholder="1 pushup a day keeps the doctor at bay"
+        class="flex-1 py-3 px-4 text-size-lg block bg-sepia focus:outline-none focus:ring-2 focus:ring-chino"
       />
-      <button type="submit">add</button>
+      <button
+        class="bg-sand appearance-none color-black block py-3 px-4 font-bold justify-self-end"
+        type="submit">add</button
+      >
     </div>
   </form>
 </div>
-
-<style>
-  .flex {
-    display: flex;
-    width: 50%;
-    min-width: 260px;
-  }
-
-  button {
-    background-color: var(--cSand);
-    appearance: none;
-    color: var(--cBlack);
-    border: none;
-    border-bottom: 2px solid var(--cChino);
-    line-height: 1;
-    display: block;
-    padding: 11px 18px;
-    font-weight: bold;
-    font-size: var(--fz-default);
-    font-family: var(--ff-system);
-    justify-self: flex-end;
-  }
-
-  input {
-    flex: 1;
-    line-height: 1;
-    padding: 11px 18px;
-    font-size: var(--fz-large);
-    display: block;
-    background-color: var(--cSepia);
-    border: none;
-    border-bottom: 2px solid var(--cChino);
-    font-family: var(--ff-system);
-  }
-
-  input:focus {
-    outline-offset: -2px;
-    outline-style: solid;
-    outline-color: var(--cChino);
-  }
-
-  label {
-    margin-bottom: 10px;
-    font-size: var(--fz-small);
-  }
-</style>
