a6444d36aa10bcc7fb5cd80b7fc0293f0f63f57c
Martin Sjåstad <m.sjastad@gmail.com>
2021-05-02 14:27:02 +0200

    Convert windi config to ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'windicss/helpers'
+import typography from 'windicss/plugin/typography'
+
+module.exports = defineConfig({
+  darkMode: 'class',
+  plugins: [typography],
+  theme: {
+    extend: {
+      colors: {
+        sepia: {
+          DEFAULT: 'hsl(48, 87%, 88%)',
+          dark: 'hsl(48, 66%, 66%)',
+        },
+        chino: 'hsl(41, 29%, 72%)',
+        sand: 'hsl(48, 87%, 91%)',
+        teal: 'hsl(187, 69%, 55%)',
+        black: 'hsl(0, 13%, 8%)',
+        grey: 'hsl(0, 9%, 76%)',
+      },
+      backgroundImage: (theme) => ({
+        dreamer: "url('/assets/dreamer-hand-plant.png')",
+      }),
+    },
+  },
+})
