018178ab35fa69c90779bd288a862366f09e8a7d
Martin Sjåstad <m.sjastad@gmail.com>
2021-05-02 15:30:18 +0200

    convert header to windi

--- src/components/AppHeader.svelte
+++ src/components/AppHeader.svelte
@@ -3,23 +3,9 @@
 </script>
 
 <div>
-  <header>
-    <h1>HABID</h1>
+  <header class="mb-2 text-size-sm">
+    <h1 class="text-size-xl">HABID</h1>
     <p>a time for everything is a place to start</p>
   </header>
   <HabitInput />
 </div>
-
-<style>
-  header {
-    margin-bottom: 7px;
-    font-size: var(--fz-small);
-  }
-  h1 {
-    font-size: var(--fz-h1);
-    margin: 0;
-  }
-  p {
-    margin: 0;
-  }
-</style>
