900c8f0d73f2e6d113c6255e9a751ba9c88aed9a
Martin Sjåstad <m.sjastad@gmail.com>
2021-05-02 15:30:24 +0200

    convert to windi

--- src/components/AddDummies.svelte
+++ src/components/AddDummies.svelte
@@ -2,10 +2,6 @@
   import { habits } from '../store/habits'
 </script>
 
-<button on:click={habits.addDummyHabits}>Populate with dummies</button>
-
-<style>
-  button {
-    margin-top: 10px;
-  }
-</style>
+<button class="mt-3" on:click={habits.addDummyHabits}
+  >Populate with dummies</button
+>
